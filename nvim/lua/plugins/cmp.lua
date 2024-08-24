6c1ee1218e21f32f045d50fde0688dd4b8805a8c
jihwooon <jihwooon@gmail.com>
2024-08-25 01:37:20 +0900

    LuaSnip과 새로운 스니펫 추가
    1. nvim/lua/plugins/cmp.lua:
    - nvim-cmp 및 관련 종속성 제거: nvim-cmp 플러그인과 이에 관련된 종속성(cmp-nvim-lsp, cmp-buffer 등)이 제거되었습니다. 이는 완성 엔진이나 접근 방식의 변경을 시사합니다.
    
    - LuaSnip 구성 변경: LuaSnip 플러그인 구성이 from_vscode 로더를 사용하고 자동 스니펫을 활성화하도록 수정되었습니다. 이는 스니펫 관리를 간소화하고 VS Code 스타일의 스니펫과의 통합을 향상시킬 수 있습니다.
    키바인딩 단순화: 스니펫 확장 및 점프에 대한 키바인딩이 luasnip 함수를 사용하여 단순화되었습니다.
    
    2. nvim/lua/snippets/lua.json:
    
    새로운 스니펫: Lua 언어에 대한 "snip1"이라는 새로운 스니펫이 추가되었습니다. 이 스니펫은 Lua 코드를 위한 간단한 템플릿을 제공합니다.

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,93 +1,36 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    lazy = true,
-    build = (not LazyVim.is_win())
-        and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+    build = (not jit.os:find("Windows"))
+        and
+        "echo -e 'NOTE: jsregexp is optional, so not a big deal if it fails to build\n'; make install_jsregexp"
         or nil,
     dependencies = {
       "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").load({ paths =
+        "~/.config/nvim/lua/snippets" })
+      end,
     },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-      require("luasnip.loaders.from_vscode").load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
-    end,
     opts = {
       history = true,
       delete_check_events = "TextChanged",
+      enable_autosnippets = true,
     },
-  },
-  {
-    "nvim-cmp",
-    version = false,
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "saadparwaiz1/cmp_luasnip",
-      "hrsh7th/cmp-emoji",
-      {
-        "garymjr/nvim-snippets",
-        opts = {
-          friendly_snippets = true,
-        },
-      },
-    },
-    opts = function()
-      local cmp = require("cmp")
-      return {
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-          { name = "emoji" },
-        }),
-        formatting = {
-          format = function(entry, vim_item)
-            -- 여기에 원하는 포맷팅 로직을 추가할 수 있습니다.
-            return vim_item
-          end,
-        },
-      }
-    end,
+    -- stylua: ignore
     keys = {
       {
-        "<Tab>",
+        "<tab>",
         function()
-          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or
+              "<tab>"
         end,
         expr = true,
         silent = true,
-        mode = { "i", "s" },
+        mode = "i",
       },
-      {
-        "<S-Tab>",
-        function()
-          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<S-Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
+      { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
+      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
     },
   },
 }
