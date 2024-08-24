e2e10c6423dc946533d073a6bc8e56f8386c91f4
jihwooon <jihwooon@gmail.com>
2024-08-24 23:21:03 +0900

    Lazy.nvim, LazyVim, 플러그인 로딩
    원래 코드는 require("config.lazy") 라인만 있었는데, 새로운 코드에서는 이 라인을 유지하면서 추가적인 로직이 도입되었습니다.
    
    환경 변수 검사:
    - 새로운 코드에서는 has 함수를 정의하여 Neovim이 현재 실행되고 있는 환경을 파악합니다.
    - has 함수는 vim.fn.has 를 이용하여 현재 시스템이 macOS인지 Windows인지 확인합니다.
    
    환경 설정 파일 로딩:
    - 환경 변수 검사 결과에 따라 config.macos 또는 config.windows lua 파일을 로딩합니다.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+local is_mac = has "macunix"
+local is_win = has "win32"
+
+if is_mac then
+  require('config.macos')
+end
+if is_win then
+  require('config.windows')
+end
+
