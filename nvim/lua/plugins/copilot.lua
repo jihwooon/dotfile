273434e24ac8659ae07b8a52621f9629802a62c2
jihwooon <jihwooon@gmail.com>
2024-08-25 02:30:35 +0900

    Github Copilot 활성화 및 옵션 설정
    - suggestion 및 panel 활성화: Copilot의 제안 기능과 패널이 활성화되어 편리하게 코드 완성을 받을 수 있습니다.
    - 다양한 옵션 설정: 제안 트리거, 키 바인딩, 패널 레이아웃 등 다양한 옵션이 사용자의 편의에 맞게 설정되었습니다.
    - 지원 파일 유형 확장: Copilot이 지원하는 파일 유형이 확장되어 더욱 다양한 환경에서 코드 완성을 받을 수 있습니다.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -4,12 +4,44 @@ return {
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = false,
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+          refresh = "gr",
+          open = "<M-CR>",
+        },
+        layout = {
+          position = "bottom", -- | top | left | right
+          ratio = 0.4,
+        },
+      },
       filetypes = {
         markdown = true,
         help = true,
+        gitcommit = true,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
       },
     },
-  }
+  },
 }
