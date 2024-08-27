cb2fa7d91e03ff370959727e002191f33c360ad2
jihwooon <jihwooon@gmail.com>
2024-08-28 01:02:19 +0900

    Wezterm 설정 업데이트
    Wezterm 설정 파일인 wezterm.lua를 업데이트하였습니다. 주로 스타일과 관련된 설정이 변경되었습니다.
    
    주요 변경사항:
    1. 문자열을 표현하는 데 사용되는 따옴표가 작은 따옴표에서 큰 따옴표로 변경되었습니다.
    2. 폰트 크기가 12.5에서 13.5로 증가하였습니다.
    3. 창 배경 투명도가 0.75에서 0.8로 증가하였습니다.
    4. 색상 스키마가 'tokyonight_moon'에서 'tokyonight'로 변경되었습니다.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,21 +2,21 @@ local wezterm = require("wezterm")
 local actions = require("actions")
 local keymaps = require("keymaps")
 
-local os = require('os')
-local home_path = os.getenv('HOME')
+local os = require("os")
+local home_path = os.getenv("HOME")
 
 return {
-  font = wezterm.font_with_fallback({ 'JetBrains Mono', 'NanumBarunGothic' }),
-  font_size = 12.5,
-  line_height = 1.2,
-  window_background_opacity = 0.75,
-  keys = keymaps,
-  automatically_reload_config = true,
-  color_scheme = 'tokyonight_moon',
-  window_background_gradient = {
-    colors = { '#000', '#002' },
-    orientation = { Linear = { angle = -45.0 } },
-    interpolation = 'Linear',
-    blend = 'Rgb',
-  },
+	font = wezterm.font_with_fallback({ "JetBrains Mono", "NanumBarunGothic" }),
+	font_size = 13.5,
+	line_height = 1.2,
+	window_background_opacity = 0.8,
+	keys = keymaps,
+	automatically_reload_config = true,
+	color_scheme = "tokyonight",
+	window_background_gradient = {
+		colors = { "#000", "#002" },
+		orientation = { Linear = { angle = -45.0 } },
+		interpolation = "Linear",
+		blend = "Rgb",
+	},
 }
